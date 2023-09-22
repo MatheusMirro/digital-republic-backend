4c0233ad7c5db33e22c76ea8a29c98556014eb6e
Matheus Alves <mirro123@outlook.com>
2023-09-22 09:30:39 -0300

    instaled bull, config app module and create a .env file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PaintController } from './paint/controllers/paint.controller';
@@ -7,5 +8,13 @@ import { PaintService } from './paint/services/paint.service';
 @Module({
   controllers: [AppController, PaintController],
   providers: [AppService, PaintService],
+  imports: [
+    BullModule.forRoot({
+      redis: {
+        host: process.env.BULL_HOST,
+        port: Number(process.env.BULL_PORT),
+      },
+    }),
+  ],
 })
 export class AppModule {}
